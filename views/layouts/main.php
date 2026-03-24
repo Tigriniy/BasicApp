e1e0d8f97a6014c84bd5666f39e5cc35deabb7d0
Tigriniy <kurinoytimofey@gmail.com>
2026-03-24 10:01:37 +0700

    Changing the main template of the HR management system

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,29 +5,39 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pop it MVC</title>
+    <title>Отдел кадров</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
 <header>
+    <h1>Система учета сотрудников отдела кадров</h1>
     <nav>
         <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
-        <?php
-        if (!app()->auth::check()):
-            ?>
+        <?php if (!app()->auth::check()): ?>
+            <a href="<?= app()->route->getUrl('/employees/add') ?>">Добавить сотрудника</a>
+            <a href="<?= app()->route->getUrl('/employees') ?>">Список сотрудников</a>
+            <a href="<?= app()->route->getUrl('/employees/by_department') ?>">По подразделениям</a>
+            <a href="<?= app()->route->getUrl('/employees/by_category') ?>">По составу</a>
+            <a href="<?= app()->route->getUrl('/departments/avg_age') ?>">Средний возраст</a>
+
+            <?php if (!app()->auth::user()->role === 'admin'): ?>
+                <a href="<?=app()->route->getUrl('/signup') ?>">Добавить кадровика</a>
+            <?php endif; ?>
+
+            <a href="<?= app()->route->getUrl('/logout') ?>">Выйти (<?= htmlspecialchars(app()->auth::user()->login ?? '') ?>)</a>
+
+        <?php else: ?>
             <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
-            <a href="<?= app()->route->getUrl('/signup') ?>">Регистрация</a>
-        <?php
-        else:
-            ?>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-        <?php
-        endif;
-        ?>
+        <?php endif; ?>
     </nav>
 </header>
 <main>
     <?= $content ?? '' ?>
 </main>
+
+<footer>
+    <p>&copy;<?= date('Y') ?>Отдел кадров</p>
+</footer>
 
 </body>
 </html>
